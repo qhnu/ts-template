f224604e0ed4fcdc13bc380d98428f66c128a3c1
qhnu <73204221+qhnu@users.noreply.github.com>
2021-02-27 15:07:56 +0900

    build: remove dist

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const sample: (a: number, b: number) => number;
-//# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sample = void 0;
-const sample = (a, b) => a + b;
-exports.sample = sample;
